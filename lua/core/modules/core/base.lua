5fe016daa793493f1339082055c809e331cf710e
comfysage <67917529+comfysage@users.noreply.github.com>
2024-04-15 17:27:46 +0200

    docs(dash): add inline docs to module config
    add documentation detailing the use of the `file_associations` field

--- lua/core/modules/core/base.lua
+++ lua/core/modules/core/base.lua
@@ -1,6 +1,8 @@
 return {
   default = {
     opts = {
+      -- `file_associations` can be used to set options for specific filetypes or
+      --  setup a custom environment when opening a specific file
       file_associations = {},
     },
   },
