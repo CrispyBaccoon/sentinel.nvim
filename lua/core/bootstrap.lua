e4a879d9513a6a97f140e6ad93f4333473999168
comfysage <67917529+comfysage@users.noreply.github.com>
2024-06-18 13:11:31 +0200

    fix(core.bootstrap): error on unmatched input

--- lua/core/bootstrap.lua
+++ lua/core/bootstrap.lua
@@ -5,8 +5,10 @@ return {
     local spec = vim.iter(core._inputs):find(function(v)
       return v.name == name
     end)
-    if spec then
-      Util.boot(spec)
+    if not spec then
+      Util.log('core.bootstrap', ('could not find input \'%s\''):format(name))
+      return
     end
+    Util.boot(spec)
   end,
 }
