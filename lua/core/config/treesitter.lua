d4c911ff0dff21876d9ff9c8b84d625de5bd5d4e
comfysage <67917529+comfysage@users.noreply.github.com>
2024-06-22 11:42:18 +0200

    fix(core.config.treesitter): overhaul treesitter config

--- lua/core/config/treesitter.lua
+++ lua/core/config/treesitter.lua
@@ -5,7 +5,7 @@ local ensure = { 'markdown', 'markdown_inline', 'vimdoc' }
 return {
   setup = function(opts)
     Util.log('treesitter.setup', 'loading treesitter.')
-    require 'core.plugins'.load 'treesitter'
+    require('core.plugins').load 'treesitter'
 
     local ok, treesitter = SR_L 'nvim-treesitter'
     if not ok then
@@ -13,12 +13,22 @@ return {
     end
     treesitter.setup()
 
-    for _, v in ipairs(ensure) do
-      opts.ensure_installed[#opts.ensure_installed + 1] = v
+    if
+      type(opts.ensure_installed) == 'string'
+      and opts.ensure_installed ~= 'all'
+    then
+      opts.ensure_installed = { opts.ensure_installed }
+    end
+    if type(opts.ensure_installed) == 'table' then
+      for _, v in ipairs(ensure) do
+        ---@diagnostic disable-next-line: assign-type-mismatch
+        opts.ensure_installed[#opts.ensure_installed + 1] = v
+      end
     end
     opts.config.ensure_installed = opts.ensure_installed
-    require 'nvim-treesitter.configs'.setup(opts.config)
+    require('nvim-treesitter.configs').setup(opts.config)
 
-    keymaps.normal['gm'] = { '<Cmd>Inspect<CR>', 'Show TS highlight groups under cursor' }
-  end
+    keymaps.normal['gm'] =
+      { vim.show_pos, 'Show TS highlight groups under cursor' }
+  end,
 }
