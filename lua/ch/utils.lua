0c671cd5d39df0651da7ce3b62762cbd4f92a00f
comfysage <67917529+comfysage@users.noreply.github.com>
2024-07-01 13:13:43 +0200

    fix(ch.utils): use `Util.log` as a temp wrapper for `ch.log`

--- lua/ch/utils.lua
+++ lua/ch/utils.lua
@@ -1,22 +1,8 @@
 local Util = {}
 
---- vim.notify wrapper to avoid msg overload
----@param msg string
----@param level 'debug'|'info'|'warn'|'error'|nil
-function Util.log(source, msg, level)
-  if ch and ch.log then
-    ch.log:write(source, msg, level)
-  else
-    vim.notify(('[%s] %s'):format(source, msg), ({
-      debug = vim.log.levels.DEBUG,
-      info = vim.log.levels.INFO,
-      warn = vim.log.levels.WARN,
-      error = vim.log.levels.ERROR,
-    })[level or 'debug'])
-    -- > [!NOTE] although debug and info logs are allowed using these before
-    -- > ch.log (before initialization) could overload the user with
-    -- > information everytime they enter neovim
-  end
+---@type fun(source: string, msg: string, level: 'debug'|'info'|'warn'|'error'|nil)
+function Util.log(...)
+  return ch.log(...)
 end
 
 return Util
